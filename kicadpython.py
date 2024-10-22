c22ef0a0d68bc483fc2398c8a4b90ad8a4c8fb4f
jackfang-0601 <jackfang0601@gmail.com>
2024-10-22 11:12:48 -0500

    Update kicadpython.py

--- kicadpython.py
+++ kicadpython.py
@@ -1,16 +1,10 @@
 from skip import Schematic
-sch = Schematic("/Users/Desktop//Untitled/Untitled.kicad_sch")#set up the path of the program 
-
-
-a = sch.symbol.R2.clone()#add a elements(You can only clone a excited elements)
-
-a.move(50,100)# move the symbol(x,y)
-
-sch.symbol.R1.Value.value = '100k' # change the value of R1
-
-
-sch.write("/Users/Desktop//Untitled/Untitled.kicad_sch")#you have to rewrite the program in order to save the changes you made
-
+sch = Schematic("/Users/Desktop//Untitled/Untitled.kicad_sch")
+a = sch.symbol.R2.clone()#add a elements
+a.move(50,100)
+sch.symbol.R1.move(100,100)
+sch.symbol.R1.Value.value = '100k' 
+sch.write("/Users/Desktop//Untitled/Untitled.kicad_sch")
 
 
                                                                                                                                           
